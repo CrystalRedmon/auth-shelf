4909634fa8c3b932de8c9803078ee0ac3ad6a1e3
Maia Johnson <maiaj1306@gmail.com>
2022-11-10 13:18:00 -0600

    fix fetch for requests

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 
 function ShelfPage() {
+  const dispatch = useDispatch();
+  const shelfItems = useSelector(store => store.items);
+  // console.log(shelfItems);
+
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_ITEMS",
+    })
+  }, []);
+
   return (
     <div className="container">
       <h2>Shelf</h2>
-      <p>All of the available items can be seen here.</p>
+      <ul>{shelfItems.map(item => (
+        <li key={item.id}>
+          <li>{item.description}</li><br></br>
+          <li>{item.image_url}</li></li>))}
+          
+      </ul>
     </div>
   );
 }
